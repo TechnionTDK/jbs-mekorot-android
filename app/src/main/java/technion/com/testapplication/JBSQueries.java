7ac251cf28081fb87c4d116da5ce1c2e4112e1f3
Tomer Levinson <tomer@insert.io>
2017-12-23 13:00:00 +0200

    Added query for mekorot from psukim.

--- app/src/main/java/technion/com/testapplication/JBSQueries.java
+++ app/src/main/java/technion/com/testapplication/JBSQueries.java
@@ -1,5 +1,7 @@
 package technion.com.testapplication;
 
+import java.util.ArrayList;
+
 /**
  * Created by tomerlevinson on 15/12/2017.
  */
@@ -11,6 +13,9 @@ public class JBSQueries {
     public static final String JBS_URI = "uri";
     public static final String PASUK = "pasuk";
     public static final String PASUK_TEXT = "pasuk_text";
+    public static final String MAKOR_NAME = "label";
+    public static final String MAKOR_TEXT = "text";
+
 
     public static final String GET_ALL_PSUKIM_FROM_PARASHA =
             "SELECT ?pasuk ?position ?pasuk_text sum(strlen(?text)) as ?total_perush_size WHERE {" +
@@ -20,13 +25,13 @@ public class JBSQueries {
                     "";
     public static final String GET_ALL_PARASHOT = "PREFIX jbr: <http://jbs.technion.ac.il/resource/>" +
             "PREFIX jbo: <http://jbs.technion.ac.il/ontology/>" +
-            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"+
-            "PREFIX dco: <http://purl.org/dc/terms/>"+
-            "SELECT ?uri ?label ?position "+
+            "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+            "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>" +
+            "PREFIX dco: <http://purl.org/dc/terms/>" +
+            "SELECT ?uri ?label ?position " +
             "WHERE {" +
             " ?uri a jbo:ParashaTorah ." +
-            " ?uri rdfs:label ?label ."+
+            " ?uri rdfs:label ?label ." +
             " ?uri jbo:position ?position" +
             " .} ORDER BY ASC(xsd:integer(?position))";
     public static final String GET_ALL_PRAKIM = "PREFIX jbr: <http://jbs.technion.ac.il/resource/> \n" +
@@ -42,21 +47,22 @@ public class JBSQueries {
             "?book a jbo:BookTanach.\n" +
             "} ORDER BY ASC(xsd:integer(?position))";
     public static final String SAMPLE_DBPEDIA_QUERY =
-            "PREFIX p: <http://dbpedia.org/property/>"+
-                    "PREFIX dbpedia: <http://dbpedia.org/resource/>"+
-                    "PREFIX category: <http://dbpedia.org/resource/Category:>"+
-                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-                    "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"+
-                    "PREFIX geo: <http://www.georss.org/georss/>"+
+            "PREFIX p: <http://dbpedia.org/property/>" +
+                    "PREFIX dbpedia: <http://dbpedia.org/resource/>" +
+                    "PREFIX category: <http://dbpedia.org/resource/Category:>" +
+                    "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
+                    "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>" +
+                    "PREFIX geo: <http://www.georss.org/georss/>" +
                     "PREFIX geo-pos: <http://www.w3.org/2003/01/geo/wgs84_pos#>" +
-                    "SELECT ?label ?lat ?long ?comment "+
-                    "WHERE {"+
-                    " ?subject geo-pos:lat ?lat ."+
-                    " ?subject geo-pos:long ?long ."+
-                    " ?subject rdfs:label ?label ."+
-                    " ?subject rdfs:comment ?comment ."+
-                    " FILTER( (?lat > 48.23185544 && ?lat < 49.23185844 && ?long > 16.31660208 && ?long < 17.31660208) && (lang(?comment) = 'en') && (lang(?label) = 'en') )"+
+                    "SELECT ?label ?lat ?long ?comment " +
+                    "WHERE {" +
+                    " ?subject geo-pos:lat ?lat ." +
+                    " ?subject geo-pos:long ?long ." +
+                    " ?subject rdfs:label ?label ." +
+                    " ?subject rdfs:comment ?comment ." +
+                    " FILTER( (?lat > 48.23185544 && ?lat < 49.23185844 && ?long > 16.31660208 && ?long < 17.31660208) && (lang(?comment) = 'en') && (lang(?label) = 'en') )" +
                     " .} ORDER BY ?lat ?long LIMIT 15 ";
+
     public static String getAllPsukimFromParashaQuery(String parashaUri) {
         return " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
                 " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -70,5 +76,90 @@ public class JBSQueries {
                 " ?pasuk a jbo:Pasuk; jbo:within jbr:" + parashaUri + "; jbo:position ?position; jbo:text ?pasuk_text.\n" +
                 " ?perush jbo:interprets ?pasuk; jbo:text ?text.\n" +
                 "} ORDER BY ASC(xsd:integer(?position))";
+    }
+
+    public static String getMekorot(ArrayList<String> psukim) {
+        String psukimList = "";
+        for (String pasuk : psukim) {
+            psukimList += pasuk + " ";
+        }
+        return  " PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>  \n" +
+                " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>  \n" +
+                " PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>  \n" +
+                " PREFIX foaf: <http://xmlns.com/foaf/0.1/>  \n" +
+                " PREFIX dc: <http://purl.org/dc/elements/1.1/>  \n" +
+                " PREFIX : <http://dbpedia.org/resource/>  \n" +
+                " PREFIX dbpedia2: <http://dbpedia.org/property/>  \n" +
+                " PREFIX dbpedia: <http://dbpedia.org/>  \n" +
+                " PREFIX skos: <http://www.w3.org/2004/02/skos/core#>  \n" +
+                " PREFIX jbr: <http://jbs.technion.ac.il/resource/>\n" +
+                " PREFIX jbo: <http://jbs.technion.ac.il/ontology/>\n" +
+                " PREFIX dco: <http://purl.org/dc/terms/>\n"
+                + "SELECT ?source ?label ?text\n"
+                + "?sums ?spanss ?labels\n"
+                + "?sum ?description\n"
+                + "WHERE {\n"
+                + " SELECT ?source ?label ?text ?description (SUM(xsd:integer(?numOfMentions)) AS ?sum)\n"
+                + " (group_concat(?numOfMentions;separator=\",\") AS ?sums)\n"
+                + " (group_concat(?spans;separator=\",\") AS ?spanss)\n"
+                + " (group_concat(?target_label ;separator=\",\") AS ?labels)\n"
+                + " WHERE { \n"
+                + "         SELECT ?mentions ?source ?numOfMentions ?target_label ?label ?text ?description\n"
+                + "         (group_concat(distinct ?span;separator=\",\") AS ?spans)\n"
+                + "         WHERE { \n"
+                + "          {\n"
+                + "                 values ?pasuk {"+ psukimList +"}\n"
+                + "                 ?pasuk a jbo:Pasuk.\n"
+                + "                 ?mentions rdf:type jbo:Mention.\n"
+                + "                 ?mentions jbo:target ?pasuk.\n"
+                + "                 ?mentions jbo:source ?source.\n"
+                + "                 ?mentions jbo:numOfMentions ?numOfMentions.\n"
+                + "                 ?mentions jbo:span ?span.\n"
+                + "                 ?source rdfs:label ?label.\n"
+                + "                 ?source jbo:text ?text.\n"
+                + "                 ?source jbo:book ?source_book.\n"
+                + "                 ?source_book jbo:description ?description.\n"
+                + "                 ?pasuk rdfs:label ?target_label }\n"
+                + "          UNION {\n"
+                + "                 values ?container {"+ psukimList + "}\n"
+                + "                 values ?types {jbo:Section jbo:ParashaTorah}\n"
+                + "                 ?container a ?types.\n"
+                + "                 ?pasuk jbo:within ?container.\n"
+                + "                 ?pasuk a jbo:Pasuk.\n"
+                + "                 ?mentions rdf:type jbo:Mention.\n"
+                + "                 ?mentions jbo:target ?pasuk.\n"
+                + "                 ?mentions jbo:source ?source.\n"
+                + "                 ?mentions jbo:numOfMentions ?numOfMentions.\n"
+                + "                 ?mentions jbo:span ?span.\n"
+                + "                 ?source rdfs:label ?label.\n"
+                + "                 ?source jbo:text ?text.\n"
+                + "                 ?source jbo:book ?source_book.\n"
+                + "                 ?source_book jbo:description ?description.\n"
+                + "                 ?pasuk rdfs:label ?target_label\n"
+                + "         }\n"
+                + "         UNION {\n"
+                + "             values ?books {"+ psukimList +"}\n"
+                + "             values ?types { jbo:BookTorah }\n"
+                + "             ?books a ?types.\n"
+                + "             ?pasuk jbo:book ?books.\n"
+                + "             ?pasuk a jbo:Pasuk.\n"
+                + "             ?mentions rdf:type jbo:Mention.\n"
+                + "             ?mentions jbo:target ?pasuk.\n"
+                + "             ?mentions jbo:source ?source.\n"
+                + "             ?mentions jbo:numOfMentions ?numOfMentions.\n"
+                + "             ?mentions jbo:span ?span.\n"
+                + "             ?source rdfs:label ?label.\n"
+                + "             ?source jbo:text ?text.\n"
+                + "             ?source jbo:book ?source_book.\n"
+                + "             ?source_book jbo:description ?description.\n"
+                + "             ?pasuk rdfs:label ?target_label\n"
+                + "         }\n"
+                + "     }\n"
+                + "     group by ?target_label ?text ?mentions ?description\n"
+                + "     ?source ?numOfMentions ?label\n"
+                + "}\n"
+                + "group by ?source ?label ?text ?description\n"
+                + "}\n"
+                + " ORDER BY DESC(?sum) offset 0 limit 20";
     }
 }
